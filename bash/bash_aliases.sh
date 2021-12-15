814b76042d90d453cb1d6d550e64e46097f5a032
yuvalmo <yuvalmoalem24@gmail.com>
2021-12-15 22:54:57 +0200

    [bash] Basic templates for bash, python and cpp

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -7,3 +7,12 @@ alias qqq='cd ../../..'
 alias qqqq='cd ../../../..'
 alias qqqqq='cd ../../../../..'
 alias qqqqqq='cd ../../../../../..'
+
+DOT=$(realpath ..)
+
+# Touch
+alias shtouch="cp $DOT/templates/tmpl.sh"
+alias pytouch="cp $DOT/templates/tmpl.py"
+alias ctouch="cp $DOT/templates/tmpl.cpp"
+
+unset DOT
---
+++ templates/tmpl.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+
+def main():
+    pass  # Code goes here...
+
+
+if __name__ == "__main__":
+    main()
